a0989370ce488812a3bbd8272b60b722bc9ffa41
Deepashika <deepashika.padmathilaka@gmail.com>
2017-01-13 14:10:57 +0530

    Update CUDA_task4.cu

--- CUDA_task4.cu
+++ CUDA_task4.cu
@@ -13,7 +13,7 @@ and functions to performe general functions.*/
 
 /*use different size matrixes (by changing the value of N) Here I have changed N value into 22*/
 
-#define N 20
+#define N 32
 
  
 /*Global function is also called "kernels".
